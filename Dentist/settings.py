cc0782d0d525fd17221b52c926e508e4d759cd3b
Elliot Davor <elliotdavor.gitmail@gmail.com>
2020-03-31 08:43:17 +0000

    django_heroku settings

--- Dentist/settings.py
+++ Dentist/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 import dj_database_url
 from decouple import config
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,7 +139,7 @@ EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=False
 # EMAIL_USE_SSL=False
 
-django-heroku.settings(locals())
+django_heroku.settings(locals())
 
 
 
