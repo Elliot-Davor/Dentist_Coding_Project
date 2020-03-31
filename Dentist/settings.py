abf19fb87b51241ce92df5099408f98e4f52587d
Elliot Davor <elliotdavor.gitmail@gmail.com>
2020-03-31 08:21:25 +0000

    tweaked for settings.y

--- Dentist/settings.py
+++ Dentist/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
 import dj_database_url
 from decouple import config
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
